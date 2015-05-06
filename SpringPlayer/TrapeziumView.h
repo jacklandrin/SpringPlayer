c18b6a0e8bc1420f8da6e364a6eb9cf593af5b2f
jacklandrin <jacklandrin@hotmail.com>
2015-05-06 14:21:59 +0800

    the theme color can change based on main color of image

---
+++ SpringPlayer/TrapeziumView.h
@@ -0,0 +1,15 @@
+//
+//  TrapeziumView.h
+//  SpringPlayer
+//
+//  Created by jack on 15/5/6.
+//  Copyright (c) 2015年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrapeziumView : UIView
+
+@property (nonatomic,strong) UIColor *color;
+
+@end
